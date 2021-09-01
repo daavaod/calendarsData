384d68c4a3e7ff058072e85a8624afe8963e4039
Danijel Avramov <danijel.avramov@gmail.com>
2021-09-01 19:12:28 +0200

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -6,3 +6,19 @@ loopObject(obj) {
     obj[key] = null
   });
 }
+
+// with array
+loopObject(obj) {
+      console.log('eventdetails before', this.eventDetails);
+      Object.keys(obj).forEach(key => {
+        if(Array.isArray(obj[key])) {
+          console.log('found array');
+          obj[key] = []
+        }
+        if (typeof obj[key] === 'object') {
+          return this.loopObject(obj[key]);
+        }
+        obj[key] = null
+      });
+      console.log('eventdetails after', this.eventDetails);
+    }
