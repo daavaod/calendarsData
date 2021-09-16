8538e4275533d91645e96428bc4aac32d732a139
Danijel Avramov <danijel.avramov@gmail.com>
2021-09-16 10:32:24 +0200

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -39,3 +39,18 @@ var result = arr.reduce((unique, o) => {
     return unique;
 },[]);
 console.log(result);
+
+// array sort
+computed: {
+  sortedArray: function() {
+    function compare(a, b) {
+      if (a.name < b.name)
+        return -1;
+      if (a.name > b.name)
+        return 1;
+      return 0;
+    }
+
+    return this.arrays.sort(compare);
+  }
+}
